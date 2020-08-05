2cb0d601cb3181cee98474f9add50ce2f8e33ae9
JinaStrickland <Jina30@gmail.com>
2020-08-05 14:48:46 -0400

    finished dog class

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+  resources :dogs, only: [:index, :show, :new, :create]
+  resources :employees, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
